4e8e970292499b1f4d717a3c890afbd5893f7ac4
ChathurangaChandrasekara <chathurangajanitha67@gmail.com>
2023-10-12 23:06:37 +0530

    added skeleton until load the page

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,38 +3,47 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform}[];
-    metacritic: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 interface FetchGameResponse {
-    count: number;
-    results: Game[];
+  count: number;
+  results: Game[];
 }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-  
-    useEffect(() => {
-        const controller = new AbortController();
-      apiClient.get<FetchGameResponse>('/games', {signal: controller.signal })
-      .then(res => setGames(res.data.results))
-      .catch(err => {
-        if(err instanceof CanceledError) return;
-        setError(err.message)});
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-      return () => controller.abort();
-    }, []);
+  useEffect(() => {
+    const controller = new AbortController();
 
-    return {games, error}
-}
+    setLoading(true);
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
 
 export default useGames;
